5c1551321a10dbb92deb5c9c3cb45f338bf2a86f
Michela Sperti <michela.sperti@gmail.com>
2020-09-10 16:18:48 +0200

    working on plot script

--- modules/style_setting.py
+++ modules/style_setting.py
@@ -6,7 +6,7 @@ class set_style():
 
     def __init__(self):
         # fonts' path
-        self.f = '/content/ADL-Book-2nd-Ed/modules/Fonts'
+        self.f = '/content/ADL-Book-2nd-Ed/modules/Fonts/Montserrat-SemiBold.ttf'
 
     def set_general_style_parameters(self):
         """Set some general style parameters for matplotlib
