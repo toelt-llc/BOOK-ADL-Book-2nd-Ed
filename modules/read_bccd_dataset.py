a572db2b363b3787207562bf2fba5073b932ce48
Michela Sperti <michela.sperti@gmail.com>
2020-09-22 15:16:19 +0200

    added plot script

--- modules/read_bccd_dataset.py
+++ modules/read_bccd_dataset.py
@@ -3,22 +3,17 @@ import xml.etree.ElementTree as ET
 from glob import glob
 import pandas as pd
 from shutil import copyfile
+import cv2
 
 class read_data():
 
     def __init__(self):
+        self.annotations_path = 'BCCD_Dataset/BCCD/Annotations/'
         self.annotations = glob('BCCD_Dataset/BCCD/Annotations/*.xml')
 
     def preprocess_bccd_dataset(self):
         """Download and preprocess the bccd dataset, generating a csv file.
 
-        ### Author/Developer: Nicolas CHEN
-        ### Filename: export.py
-        ### Version: 1.0
-        ### Field of research: Deep Learning in medical imaging
-        ### Purpose: This Python script creates the CSV file from XML files.
-        ### Output: This Python script creates the file "test.csv"
-        ### with all data needed: filename, class_name, x1,y1,x2,y2
         ### HISTORY
         ### Version | Date          | Author       | Evolution
         ### 1.0     | 17/11/2018    | Nicolas CHEN | Initial version
@@ -45,3 +40,40 @@ class read_data():
         data = pd.DataFrame(df, columns = ['filename', 'cell_type', 'xmin', 'xmax', 'ymin', 'ymax'])
 
         return data
+
+    def plot_bccd_dataset(self,imagename):
+        """Generate labeled img like example.jpg from the bccd dataset."""
+        image = cv2.imread('BCCD_Dataset/BCCD/JPEGImages/' + imagename)
+        tree = ET.parse(self.annotations_path + imagename)
+        for elem in tree.iter():
+        	if 'object' in elem.tag or 'part' in elem.tag:
+        		for attr in list(elem):
+        			if 'name' in attr.tag:
+        				name = attr.text
+        			if 'bndbox' in attr.tag:
+        				for dim in list(attr):
+        					if 'xmin' in dim.tag:
+        						xmin = int(round(float(dim.text)))
+        					if 'ymin' in dim.tag:
+        						ymin = int(round(float(dim.text)))
+        					if 'xmax' in dim.tag:
+        						xmax = int(round(float(dim.text)))
+        					if 'ymax' in dim.tag:
+        						ymax = int(round(float(dim.text)))
+        				if name[0] == "R":
+        					cv2.rectangle(image, (xmin, ymin),
+        								(xmax, ymax), (0, 255, 0), 1)
+        					cv2.putText(image, name, (xmin + 10, ymin + 15),
+        							cv2.FONT_HERSHEY_SIMPLEX, 1e-3 * image.shape[0], (0, 255, 0), 1)
+        				if name[0] == "W":
+        					cv2.rectangle(image, (xmin, ymin),
+        								(xmax, ymax), (0, 0, 255), 1)
+        					cv2.putText(image, name, (xmin + 10, ymin + 15),
+        							cv2.FONT_HERSHEY_SIMPLEX, 1e-3 * image.shape[0], (0, 0, 255), 1)
+        				if name[0] == "P":
+        					cv2.rectangle(image, (xmin, ymin),
+        								(xmax, ymax), (255, 0, 0), 1)
+        					cv2.putText(image, name, (xmin + 10, ymin + 15),
+        							cv2.FONT_HERSHEY_SIMPLEX, 1e-3 * image.shape[0], (255, 0, 0), 1)
+
+        cv2.imshow("test", image)
