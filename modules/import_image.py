3fe8a582702ccb1179f3e7179c232332d498c692
Michela Sperti <michela.sperti@gmail.com>
2020-10-28 10:56:53 +0100

    added display_img module

---
+++ modules/import_image.py
@@ -0,0 +1,12 @@
+from IPython.display import Image
+
+class display_image():
+
+    def __init__(self):
+        # images' path
+        self.img_path = 'ADL-Book-2nd-Ed/images/chapter11/'
+
+    def display_img(self,img):
+        """Display an image present in the GitHub repository."""
+        pil_img = Image(filename = self.img_path + img)
+        display(pil_img)
