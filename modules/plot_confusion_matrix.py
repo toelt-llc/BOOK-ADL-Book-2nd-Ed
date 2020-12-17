24eb68b7ebdca5abe45c0f784a2bd9c6389917fc
Michela Sperti <michela.sperti@gmail.com>
2020-12-17 15:38:23 +0100

    added module for plotting CM

--- modules/plot_confusion_matrix.py
+++ modules/plot_confusion_matrix.py
@@ -6,7 +6,7 @@ class plot_cm():
     def __init__(self):
         pass
 
-    def plot_confmat(data, labels, output_filename):
+    def plot_confmat(self, data, labels, output_filename):
         """
         Plots a confusion matrix using heatmap.
 
@@ -16,19 +16,19 @@ class plot_cm():
             output_filename (str): path to output file.
         """
 
-        sns.set(color_codes=True)
-        sns.set(font_scale=1.3)
+        sns.set(color_codes = True)
+        sns.set(font_scale = 1.3)
 
-        plt.figure(1, figsize=(9, 6))
+        plt.figure(1, figsize = (9, 6))
 
-        ax = sns.heatmap(data, annot=True, cmap='Blues', cbar_kws={'label': 'Scale'}, fmt='d')
+        ax = sns.heatmap(data, annot = True, cmap = 'Blues', cbar_kws = {'label': 'Scale'}, fmt = 'd')
 
-        ax.set_xticklabels(labels, fontsize=16)
-        ax.set_yticklabels(labels, fontsize=16)
+        ax.set_xticklabels(labels, fontsize = 16)
+        ax.set_yticklabels(labels, fontsize = 16)
 
-        ax.set_xlabel('Predicted Label', fontsize=16)
-        ax.set_xlabel('True Label', fontsize=16)
+        ax.set_xlabel('Predicted Label', fontsize = 16)
+        ax.set_xlabel('True Label', fontsize = 16)
 
-        plt.savefig(output_filename, bbox_inches='tight', dpi=300)
+        plt.savefig(output_filename, bbox_inches = 'tight', dpi = 300)
         plt.show()
         plt.close()
